8a35f9a5b6d8d572d8b3a326983d20f8024c9b01
Thiago Costa de Souza <thgsouza@outlook.com>
2022-11-09 15:32:26 -0300

    Fix bug in button background color

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,21 +1,22 @@
 let rating = null;
 let thankYouState = false;
-const ratingDiv = document.getElementById("rating-box")
-const thankYouDiv = document.getElementById("thank-you")
-const ratingFinal = document.getElementById("rating")
+const ratingDiv = document.getElementById("rating-box");
+const thankYouDiv = document.getElementById("thank-you");
+const ratingFinal = document.getElementById("rating");
 
 function setRating(id) {
   if (rating != null) {
     const oldButton = document.getElementById(`${rating}`);
-    oldButton.classList.remove("bg-primary-orange", "text-secondary-0")
+    oldButton.classList.remove("bg-primary-orange", "text-secondary-0");
+    oldButton.classList.add("bg-secondary-400/10");
   }
 
   rating = id;
   const button = document.getElementById(`${rating}`);
+  button.classList.remove("bg-secondary-400/10");
   button.classList.add("bg-primary-orange", "text-secondary-0");
-
   const submitBtn = document.getElementById("submit");
-  submitBtn.removeAttribute('disabled');
+  submitBtn.removeAttribute("disabled");
 }
 
 function submitRating() {
